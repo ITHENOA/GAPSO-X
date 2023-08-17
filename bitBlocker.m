94eec0f0f2cb04d0eb472604ec4d41478a3bd0bc
ITHENOA <79358150+ITHENOA@users.noreply.github.com>
2023-08-17 18:52:59 +0330

    2

--- bitBlocker.m
+++ bitBlocker.m
@@ -1,14 +1,14 @@
-function [prob,dist] = bitBlocker(prob,center,bound,resolution,grid)
+function prob = bitBlocker(prob,center,bound,resolution,grid)
 
 bit = abs(bound(:,2)-bound(:,1))./resolution';
-sigma = ceil(resolution'/100) .* bit;
+sigma = ceil(resolution'/10) .* bit;
 dim = size(bound,1);
 
 dist = cell(dim);
 for i = 1:dim-1
     for j = i+1:dim
-        dist1 = exp(-((grid{i,j}'-center(i))/sigma(i)).^2); % Gaussian   X'??
-        dist2 = exp(-((grid{j,i}'-center(j))/sigma(j)).^2); % Gaussian   Y'??
+        dist1 = exp(-((grid{i,j}' - center(i))/sigma(i)).^2); % Gaussian   X'??
+        dist2 = exp(-((grid{j,i}' - center(j))/sigma(j)).^2); % Gaussian   Y'??
         dist{i,j} = dist1 .* dist2;
         prob{i,j} = prob{i,j} - dist{i,j};
         prob{i,j}(prob{i,j} < 0) = 0;
---
+++ isinrange.m
@@ -0,0 +1,11 @@
+function out = isinrange(x,range)
+
+out = ones(1,numel(x));
+for i = 1:length(x)
+    if x(i) >= range(i,1) && x(i) <= range(i,2)
+        out(i) = true;
+    else
+        out(i) = false;
+    end
+    out = prod(out);
+end
